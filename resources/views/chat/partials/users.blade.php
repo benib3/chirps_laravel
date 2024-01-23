1f42d567acd852c961e4710ee7e86ec7dd7e5ce2
benib3 <brate.tebra@live.com>
2024-01-23 22:03:27 +0100

    link added

--- resources/views/chat/partials/users.blade.php
+++ resources/views/chat/partials/users.blade.php
@@ -5,21 +5,23 @@
    <div class="flow-root">
         <ul role="list" class="divide-y divide-gray-200 dark:divide-gray-700">
             @foreach ($users as $user)
-            <li class="py-3 sm:py-4">
-                <div class="flex items-center">
-                    <div class="flex-shrink-0">
-                        <img class="w-8 h-8 rounded-full" src="{{ asset('storage/images/' .  $user->img) }}" alt="Profile image">
+            <a href="{{ route('chirps.chat.show', $user->id) }}">
+                <li class="py-3 sm:py-4 hover:bg-gray-700 border-gray-800 rounded-lg">
+                    <div class="flex items-center">
+                        <div class="flex-shrink-0">
+                            <img class="w-8 h-8 rounded-full" src="{{ asset('storage/images/' .  $user->img) }}" alt="Profile image">
+                        </div>
+                        <div class="flex-1 min-w-0 ms-4">
+                            <p class="text-sm font-medium text-gray-900 truncate dark:text-white">
+                                {{$user->name}}
+                            </p>
+                            <p class="text-sm text-gray-500 truncate dark:text-gray-400">
+                                {{$user->email}}
+                            </p>
+                        </div>
                     </div>
-                    <div class="flex-1 min-w-0 ms-4">
-                        <p class="text-sm font-medium text-gray-900 truncate dark:text-white">
-                            {{$user->name}}
-                        </p>
-                        <p class="text-sm text-gray-500 truncate dark:text-gray-400">
-                            {{$user->email}}
-                        </p>
-                    </div>
-                </div>
-            </li>
+                </li>
+            </a>
             @endforeach
         </ul>
    </div>
